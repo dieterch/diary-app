9a19ef4d8f0180019d0f5d52413965595176194b
dieterch <dieter.chvatal@gmail.com>
2025-12-11 14:17:42 +0000

    icons added

--- app/app.vue
+++ app/app.vue
@@ -12,3 +12,24 @@ html, body {
   font-family: inherit !important;
 }
 </style>
+<script setup>
+useHead({
+  link: [
+    // Standard favicon
+    { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+
+    // Apple iPhone / iPad icon
+    { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" },
+
+  ],
+
+  meta: [
+    // iOS status bar color
+    { name: "apple-mobile-web-app-capable", content: "yes" },
+    { name: "apple-mobile-web-app-status-bar-style", content: "black-translucent" },
+
+    // Android theme color
+    { name: "theme-color", content: "#1e5f8b" }
+  ]
+})
+</script>
